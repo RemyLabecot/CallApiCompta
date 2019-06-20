714bb6c2c04643f4d326576c819f119d6568d289
RemyLabecot <remylabecot@gmail.com>
2019-06-20 15:05:21 +0200

    refactoring service invert Date, add error on search and optimize code

--- src/app/operation-list/operation-list.component.ts
+++ src/app/operation-list/operation-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OperationService } from '../operation.service';
 import { IOperation } from '../operation';
-import { map, groupBy } from 'rxjs/operators';
 import { DatePipe } from '@angular/common';
+import { IStatut } from '../statut';
 
 @Component({
   selector: 'app-operation-list',
@@ -16,36 +16,26 @@ export class OperationListComponent implements OnInit {
   public ribSelected: string;
   public dateMin: string;
   public dateMax: string;
+  public message: string;
 
   constructor(private _operationService: OperationService, public datepipe: DatePipe) { }
 
   ngOnInit() {
-    this._operationService.getOperations()
-      .subscribe(data => {
-        this.operations = data.operations;
-      });
   }
 
-  getSelectedRib() {
-    this._operationService.getOperations()
-      .subscribe(data => {
+  getOperationsByRibAndDate() {
+
+    this._operationService.getOperations().subscribe(
+      data => {
         this.operations = data.operations;
-        this.operationSelected = this.operations
-          .filter(x =>
+        this.operationSelected = this.operations.
+          filter(x =>
             x.RIB === this.ribSelected &&
-              (this.invertDateFormat(x.Date) > this.dateMin && this.invertDateFormat(x.Date) < this.dateMax));
-      });
-  }
-
-  invertDateFormat(date: string): string {
-
-    // come with 24/04/2017 format
-    let dd: string;
-    dd = date.substring(0, 2);
-    let mm: string;
-    mm = date.substring(3, 5);
-    const yyyy = date.substring(6, 10);
-    const newStringDate = yyyy + '-' + mm + '-' + dd;
-    return newStringDate;
+            (this._operationService.invertDateFormat(x.Date) > this.dateMin
+              && this._operationService.invertDateFormat(x.Date) < this.dateMax));
+      },
+      err => {this.message = 'BOUM'
+      console.log('Erreur ' + err);}
+    );
   }
 }
--- src/app/operation.service.ts
+++ src/app/operation.service.ts
@@ -2,16 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IStatut } from './statut';
-import { map } from 'rxjs/operators';
 
 @Injectable()
 export class OperationService {
 
     private _url: string = 'http://agrcf.lib.id/exercice@dev/';
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) { }
 
     getOperations(): Observable<IStatut> {
         return this.http.get<IStatut>(this._url);
+    }
+
+    invertDateFormat(date: string): string {
+        // come with 24/04/2017 format
+        let dd: string;
+        dd = date.substring(0, 2);
+        let mm: string;
+        mm = date.substring(3, 5);
+        const yyyy = date.substring(6, 10);
+        const newStringDate = yyyy + '-' + mm + '-' + dd;
+        return newStringDate;
     }
 }
